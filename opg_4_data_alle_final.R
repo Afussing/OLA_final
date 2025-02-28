00f294a2b2233d4d31e818cee86157cec766a4e2
Afussing98 <Andreas.fussing.hansen@gmail.com>
2025-02-28 09:48:54 +0100

    first commit

---
+++ opg_4_data_alle_final.R
@@ -0,0 +1,178 @@
+library(tidyverse)
+library(ggsoccer)
+library(stringr)
+library(mongolite)
+library(jsonlite)
+
+#### Opgave 4.1 ####
+# Generelt for alle skal der establish en connection først:
+
+conmatches <- mongo(collection = "matches", 
+                    db = "NewWyscout", 
+                    url = "mongodb://localhost"
+)
+
+congames <- mongo(collection = "games", 
+                  db = "NewWyscout", 
+                  url = "mongodb://localhost")
+
+conspiller <- mongo(collection = "players", 
+                    db = "NewWyscout", 
+                    url = "mongodb://localhost")
+
+#############################
+###### Data opgave 4.1 ######
+#############################
+# Lav en dynamisk webapp hvor man ser et bar-plot af spillerens samlede målscore og xG-sum 
+# og hvor man kan regulere antallet af spillere. I skal vedlægge koden. Det gælder for alle tre opgaver.
+
+# Polen
+dfmål <- as.data.frame(flatten(congames$find('{"shot.isGoal": true}'))) 
+
+df_polen_label <- as.data.frame(flatten(conmatches$find(query = '{"competitionId": 692}',
+                                                        fields = '{}')))
+colnames(df_polen_label)[1] = "matchId"
+dfmål_polen <- inner_join(dfmål, df_polen_label, by = "matchId")
+
+### Opretter nu kolonne med sæson
+# Først sorteres kolonne data, så tidspunkt ikke fremgår og sæson-kolonne oprettes
+
+dfmål_polen$date <- as.Date(gsub(" \\d{2}:\\d{2}:\\d{2}$", "", dfmål_polen$date))
+
+dfmål_polen$sæson <- ifelse(dfmål_polen$date < as.Date("2022-05-22"), "21/22", "22/23")
+
+dfmål_polen <- dfmål_polen %>% mutate(
+  player.name.final = paste0(player.name, " - ", team.name)
+)
+
+# Holland
+df_holland_label <- as.data.frame(flatten(conmatches$find(query = '{"competitionId": 635}',
+                                                          fields = '{}')))
+colnames(df_holland_label)[1] = "matchId"
+dfmål_holland <- inner_join(dfmål, df_holland_label, by = "matchId")
+
+### Opretter nu kolonne med sæson
+# Først sorteres kolonne data, så tidspunkt ikke fremgår og sæson-kolonne oprettes
+
+dfmål_holland$date <- as.Date(gsub(" \\d{2}:\\d{2}:\\d{2}$", "", dfmål_holland$date))
+
+dfmål_holland$sæson <- ifelse(dfmål_holland$date < as.Date("2022-05-16"), "21/22", "22/23")
+
+dfmål_holland <- dfmål_holland %>% mutate(
+  label_final = paste0(date,": ", label)
+)
+
+dfmål_holland <- dfmål_holland %>% mutate(
+  player.name.final = paste0(player.name, " - ", team.name)
+)
+
+
+#############################
+###### Data opgave 4.2 ######
+#############################
+
+# Polen
+
+### Dataframes Matches og Games sammensættes ###
+
+df_polen_vl <- paste0(df_polen_label$matchId, collapse = ",")
+
+query_polen <- paste0('{"type.primary": "pass", "matchId": {"$in": [',df_polen_vl,'] }}')
+
+dfpolen <- congames$find(query = query_polen,
+                         fields = '{}')
+
+df_polen <- as.data.frame(flatten(dfpolen))
+
+df_polen <- inner_join(df_polen,df_polen_label, by = "matchId")
+
+### Opretter nu kolonne med sæson
+# Først sorteres kolonne data, så tidspunkt ikke fremgår og sæson-kolonne oprettes
+
+df_polen$date <- as.Date(gsub(" \\d{2}:\\d{2}:\\d{2}$", "", df_polen$date))
+
+df_polen$sæson <- ifelse(df_polen$date < as.Date("2022-05-22"), "21/22", "22/23")
+
+# Label sammensættes med dato
+
+df_polen <- df_polen %>% mutate(
+  label_final = paste0(date,": ",label)
+)
+
+df_polen <- df_polen %>% mutate(
+  Hjemmebane = str_extract(label, "^[^\\-]+") %>% str_trim(), #  Beholder alle ord før det første "-"
+  Udebane = str_extract(label, "(?<=-\\s)[^\\d]+") %>% str_trim()
+)
+
+# Holland
+
+
+### Dataframes Matches og Games sammensættes ###
+
+
+holland_vl <- paste0(df_holland_label$matchId, collapse = ",")
+query_holland <- paste0('{"type.primary": "pass", "matchId": {"$in": [',holland_vl,'] }}')
+
+dfholland <- congames$find(query = query_holland,
+                         fields = '{}')
+
+df_holland <- as.data.frame(flatten(dfholland))
+
+df_holland <- inner_join(df_holland,df_holland_label, by = "matchId")
+
+### Opretter nu kolonne med sæson
+# Først sorteres kolonne data, så tidspunkt ikke fremgår og sæson-kolonne oprettes
+
+df_holland$date <- as.Date(gsub(" \\d{2}:\\d{2}:\\d{2}$", "", df_holland$date))
+
+df_holland$sæson <- ifelse(df_holland$date < as.Date("2022-05-22"), "21/22", "22/23")
+
+# Label sammensættes med dato
+
+df_holland <- df_holland %>% mutate(
+  label_final = paste0(date,": ",label)
+)
+
+df_holland <- df_holland %>% mutate(
+  Hjemmebane = str_extract(label, "^[^\\-]+") %>% str_trim(), #  Beholder alle ord før det første "-"
+  Udebane = str_extract(label, "(?<=-\\s)[^\\d]+") %>% str_trim()
+)
+
+
+#############################
+###### Data opgave 4.3 ######
+#############################
+
+# Lav en dynamisk webapp vha ggsoccer, hvor man kan vælge en kamp og derpå se et plot af skud og hvor fordelingen mellem de to hold vises vha farve.
+# Desuden skal man vha farven kunne se hvilke skud der gik i mål. 
+
+### Forberedelse af data.frames
+
+# Polen
+dfskud_polen <- as.data.frame(flatten(congames$find('{"type.primary": { "$in": ["shot", "penalty"]}}')))
+dfskud_polen <- inner_join(dfskud_polen,df_polen_label, by = "matchId")
+
+dfskud_polen <- dfskud_polen %>% mutate(
+  Hjemmebane = str_extract(label, "^[^\\-]+") %>% str_trim(), #  Beholder alle ord før det første "-"
+  Udebane = str_extract(label, "(?<=-\\s)[^\\d]+") %>% str_trim() %>% gsub(",", "", .)
+)
+unique(dfskud_polen$type.primary)
+dfskud_polen$type.primary <- gsub("penalty", "shot", dfskud_polen$type.primary)
+
+# Holland
+dfskud_holland <- as.data.frame(flatten(congames$find('{"type.primary": { "$in": ["shot", "penalty"]}}')))
+dfskud_holland <- inner_join(dfskud_holland,df_holland_label, by = "matchId")
+
+dfskud_holland <- dfskud_holland %>% mutate(
+  Hjemmebane = str_extract(label, "^[^\\-]+") %>% str_trim(),
+  Udebane = str_extract(label, "(?<=-\\s)[^\\d]+") %>% str_trim() %>% gsub(",", "", .)
+)
+
+dfskud_holland$type.primary <- gsub("penalty", "shot", dfskud_holland$type.primary)
+
+
+# Generelt
+## Bruges til opdeling af fodboldbanen 
+
+pitch_length <- 100
+pitch_width <- 100
---
+++ opg_4_shiny_final.R
@@ -0,0 +1,354 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(shinydashboard)
+library(ggsoccer)
+library(plotly)
+library(tidyr)
+
+# Pitch dimensioner
+pitch_length <- 100
+pitch_width <- 100
+
+# UI
+ui <- dashboardPage(
+  dashboardHeader(title = "Kombineret Shiny Applikation"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Mål per spiller", tabName = "mål_per_spiller", icon = icon("chart-bar")),
+      menuItem("Spilninger per kamp", tabName = "spilninger_per_kamp", icon = icon("passenger-car")),
+      menuItem("Skud Visualisering", tabName = "skud_visualisering", icon = icon("bullseye"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # Pane 1: Mål per spiller
+      tabItem(tabName = "mål_per_spiller",
+              fluidRow(
+                box(title = "Mål per spiller", status = "primary", solidHeader = TRUE, width = 12,
+                    sidebarLayout(
+                      sidebarPanel(
+                        radioButtons("sæson_valg", label = "Vælg sæson",
+                                     choices = c("Alle" = "alle", "21/22" = "21/22", "22/23" = "22/23"),
+                                     selected = "alle"),
+                        selectInput("land_valg_1", label = "Vælg et land", choices = c("Holland", "Polen")),
+                        uiOutput("team_select_ui"),
+                        uiOutput("Goal_filter_ui")
+                      ),
+                      mainPanel(
+                        plotOutput("plot_1", height = "600px", width = "1050px")
+                      )
+                    )
+                )
+              )
+      ),
+      
+      # Pane 2: Spilninger per kamp
+      tabItem(tabName = "spilninger_per_kamp",
+              fluidRow(
+                box(title = "Spilninger per kamp", status = "primary", solidHeader = TRUE, width = 12,
+                    sidebarLayout(
+                      sidebarPanel(
+                        selectInput("land_valg", label = "Vælg et land", choices = c("Holland", "Polen")),
+                        uiOutput("team_filter_ui"),
+                        uiOutput("Pass_filter_ui")
+                      ),
+                      mainPanel(
+                        plotlyOutput("plot_2", height = "600px", width = "1050px"),
+                        fluidRow(
+                          column(12, style = "margin-left: 0px;", tableOutput("spilninger_table"))
+                        )
+                      )
+                    )
+                )
+              )
+      ),
+      
+      # Pane 3: Skud Visualisering
+      tabItem(tabName = "skud_visualisering",
+              fluidRow(
+                box(title = "Skud Visualisering", status = "primary", solidHeader = TRUE, width = 12,
+                    sidebarLayout(
+                      sidebarPanel(
+                        selectInput("land_valg_2", label = "Vælg et land", choices = c("Holland", "Polen")),
+                        uiOutput("hold_filter_ui"),
+                        uiOutput("Kamp_filter_ui")
+                      ),
+                      mainPanel(
+                        plotlyOutput("plot_3", height = "600px", width = "1000px"),
+                        fluidRow(
+                          column(12, style = "margin-left: 200px;", tableOutput("skud_table"))
+                        )
+                      )
+                    )
+                )
+              )
+      )
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+  
+  # Pane 1: Mål per spiller server logik
+  output$Goal_filter_ui <- renderUI({
+    kamp_data <- if (input$land_valg_1 == "Holland") dfmål_holland else dfmål_polen
+    
+    num_players <- kamp_data %>%
+      group_by(player.name.final) %>%
+      summarise(Goals = sum(shot.isGoal, na.rm = TRUE)) %>%
+      arrange(desc(Goals)) %>%
+      nrow()
+    
+    max_players <- min(num_players, 20)  # Begrænser til maks 20 spillere
+    
+    sliderInput("Goal_filter", label = "Vælg antal spillere", 
+                min = 1, max = max_players, value = 10, step = 1)
+  })
+  
+  
+  output$team_select_ui <- renderUI({
+    kamp_data <- if (input$land_valg_1 == "Holland") dfmål_holland else dfmål_polen
+    teams <- unique(kamp_data$team.name)
+    teams <- c("Alle hold", teams)
+    
+    selectInput("team_valg", label = "Vælg et hold", choices = teams, selected = "Alle hold")
+  })
+  
+  filtered_data_1 <- reactive({
+    req(input$Goal_filter)
+    kamp_data <- if (input$land_valg_1 == "Holland") dfmål_holland else dfmål_polen
+    
+    if (input$sæson_valg != "alle" & "sæson" %in% colnames(kamp_data)) {
+      kamp_data <- kamp_data %>% filter(sæson == input$sæson_valg)
+    }
+    
+    if (input$team_valg != "Alle hold") {
+      kamp_data <- kamp_data %>% filter(team.name == input$team_valg)
+    }
+    
+    kamp_data %>%
+      group_by(player.name) %>%
+      summarise(
+        Goals = sum(shot.isGoal, na.rm = TRUE), 
+        xG = sum(shot.postShotXg, na.rm = TRUE)
+      ) %>%
+      arrange(desc(Goals)) %>%
+      head(input$Goal_filter)  # Beholder kun de øverste N spillere
+  })
+  
+  
+  output$plot_1 <- renderPlot({
+    data <- filtered_data_1()  # Hent de filtrerede data først
+    
+    ggplot(data, aes(x = reorder(player.name, -Goals), y = Goals)) +
+      geom_bar(stat = "identity") +
+      geom_point(aes(y = xG, color = "xG Sum"), size = 3) +
+      scale_y_continuous(breaks = seq(0, max(data$Goals, na.rm = TRUE), by = 2)) +  
+      labs(title = "Mål per spiller", x = "", y = "Antal mål") +
+      theme_minimal() +
+      theme(
+        axis.text.x = element_text(angle = 70, hjust = 1, size = 13),
+        axis.text.y = element_text(size = 12),
+        plot.title = element_text(size = 16, face = "bold"),
+        panel.grid.major = element_blank(),  # Fjerner store gridlines
+        panel.grid.minor = element_blank(),  # Fjerner små gridlines
+        panel.background = element_blank()   # Fjerner baggrundsfarven
+      ) +
+      guides(color = guide_legend(title = NULL))
+  })
+  
+  
+  # Pane 2: Spilninger per kamp server logik
+  output$team_filter_ui <- renderUI({
+    req(input$land_valg)
+    
+    kamp_data <- if (input$land_valg == "Holland") df_holland else df_polen
+    teams <- unique(kamp_data$team.name)
+    
+    selectInput("team_filter", label = "Vælg et hold", choices = c("Alle hold", teams))
+  })
+  
+  output$Pass_filter_ui <- renderUI({
+    req(input$team_filter)
+    
+    kamp_data <- if (input$land_valg == "Holland") df_holland else df_polen
+    games <- kamp_data %>%
+      filter(team.name == input$team_filter | input$team_filter == "Alle hold") %>%
+      pull(label_final) %>%
+      unique()
+    
+    selectInput("Pass_filter", label = "Vælg kamp", choices = games)
+  })
+  
+  filtered_data_2 <- reactive({
+    req(input$Pass_filter, input$team_filter)
+    kamp_data <- if (input$land_valg == "Holland") df_holland else df_polen
+    
+    kamp_data %>%
+      filter(label_final == input$Pass_filter) %>%
+      group_by(team.name) %>%
+      summarise(
+        good = sum(pass.accurate == TRUE, na.rm = TRUE),  
+        inaccurate = sum(pass.accurate == FALSE, na.rm = TRUE)
+      ) %>%
+      mutate(total = good + inaccurate,
+             good_pct = good / total * 100,
+             inaccurate_pct = inaccurate / total * 100) %>%
+      pivot_longer(cols = c(good, inaccurate), 
+                   names_to = "metric", values_to = "value") %>%
+      mutate(percentage = ifelse(metric == "good", good_pct, inaccurate_pct))
+  })
+  
+  output$plot_2 <- renderPlotly({
+    data_to_plot <- filtered_data_2()
+    
+    p <- ggplot(data_to_plot, aes(x = team.name, y = value, fill = metric)) +
+      geom_bar(stat = "identity", position = "stack") +
+      geom_text(aes(label = paste0(round(percentage, 1), "%")), 
+                position = position_stack(vjust = 0.5), size = 8) +
+      labs(title = "Statistik over antal spilninger", x = "", y = "Antal afleveringer", fill = "") +
+      theme(legend.title = element_blank()) +
+      scale_y_continuous(breaks = seq(0, 1000, by = 50)) +
+      theme_minimal()+
+      theme(
+        panel.grid.major.x = element_blank(),  # Fjerner gridlines fra x-aksen
+        panel.grid.major.y = element_line(color = "grey80"),  # Beholder gridlines fra y-aksen
+        panel.grid.minor = element_blank(),  # Fjerner mindre gridlines
+        panel.background = element_blank(),  # Fjerner baggrundsfarven
+        plot.title = element_text(size = 14, face = "bold"),  # Gør titlen større og bold
+        legend.title = element_blank()
+      )
+    
+    ggplotly(p)
+  })
+  
+  filtered_data_4 <- reactive({
+    req(input$Pass_filter, input$team_filter)
+    kamp_data <- if (input$land_valg == "Holland") df_holland else df_polen
+    
+    kamp_data_filtered <- kamp_data %>%
+      filter(label_final == input$Pass_filter)
+    
+    total_afleveringer <- kamp_data_filtered %>%
+      summarise(total = n()) %>%
+      pull(total)
+    
+    kamp_data %>%
+      filter(label_final == input$Pass_filter) %>%
+      group_by(team.name) %>%
+      summarise(
+        `Spilninger total` = n(),
+        `Præcise afleveringer` = sum(pass.accurate == TRUE, na.rm = TRUE),
+        `Fejl i afleveringer` = sum(pass.accurate == FALSE, na.rm = TRUE),
+        `Præcision (%)` = `Præcise afleveringer` / `Spilninger total` * 100,
+        `Andel af total afleveringer (%)` = (`Spilninger total` / total_afleveringer) * 100
+      )
+  })
+  
+  output$spilninger_table <- renderTable({
+    filtered_data_4()
+  })
+  
+  # Pane 3: Skud Visualisering server logik
+  output$hold_filter_ui <- renderUI({
+    req(input$land_valg_2)
+    
+    kamp_data <- if (input$land_valg_2 == "Holland") dfskud_holland else dfskud_polen
+    teams <- unique(kamp_data$team.name)
+    
+    selectInput("Hold_filter", label = "Vælg et hold", choices = teams)
+  })
+  
+  output$Kamp_filter_ui <- renderUI({
+    req(input$Hold_filter)
+    
+    kamp_data <- if (input$land_valg_2 == "Holland") dfskud_holland else dfskud_polen
+    games <- kamp_data %>%
+      filter(team.name == input$Hold_filter) %>%
+      pull(label) %>%
+      unique()
+    
+    selectInput("Kamp_filter", label = "Vælg kamp", choices = games)
+  })
+  
+  filtered_data_3 <- reactive({
+    req(input$Kamp_filter, input$Hold_filter)
+    kamp_data <- if (input$land_valg_2 == "Holland") dfskud_holland else dfskud_polen
+    
+    kamp_data %>%
+      filter(label == input$Kamp_filter) %>%
+      filter(!is.na(location.x) & !is.na(location.y)) %>%
+      mutate(
+        x_adj = ifelse(team.name != Hjemmebane, pitch_length - location.x, location.x),
+        y_adj = ifelse(team.name != Udebane, pitch_width - location.y, location.y),
+        shot_type = case_when(
+          shot.isGoal == TRUE ~ "Mål",
+          shot.onTarget == TRUE ~ "Skud på mål",
+          shot.onTarget == FALSE ~ "Forsøg",
+          TRUE ~ "Ukendt"
+        ),
+        color = case_when(
+          shot_type == "Mål" ~ "orange",
+          shot_type == "Skud på mål" ~ "blue",
+          shot_type == "Forsøg" ~ "red",
+          TRUE ~ "grey"
+        )
+      )
+  })
+  
+  output$plot_3 <- renderPlotly({
+    data <- filtered_data_3() %>%
+      filter(!is.na(player.name), !is.na(x_adj), !is.na(y_adj)) %>%
+      mutate(shot_type = factor(shot_type, levels = c("Mål", "Skud på mål", "Forsøg"))) 
+    
+    p <- ggplot() +
+      annotate_pitch(fill = "darkgreen", colour = "white") +
+      geom_point(data = data, aes(x = x_adj, y = y_adj, color = shot_type, text = player.name), size = 3) +
+      scale_color_manual(
+        values = c("Mål" = "orange", "Skud på mål" = "blue", "Forsøg" = "red"),
+        labels = c("Mål", "Skud på mål", "Forsøg")
+      ) +
+      theme_pitch() +
+      theme(legend.title = element_blank()) +
+      guides(color = guide_legend(title = NULL)) +
+      annotate("text", x = (pitch_length - 15), y = -2, label = unique(data$Hjemmebane)[1], color = "black", size = 5, hjust = 1) +
+      annotate("text", x = 15, y = -2, label = unique(data$Udebane)[1], color = "black", size = 5, hjust = 0)
+    
+    p_plotly <- ggplotly(p)
+    
+    p_plotly %>% layout(
+      legend = list(
+        orientation = "h",
+        x = 0.5,
+        xanchor = "center",
+        y = 0.95,
+        yanchor = "bottom"
+      )
+    )
+  })
+  
+  filtered_data_4_skud <- reactive({
+    req(input$Kamp_filter, input$Hold_filter)
+    kamp_data <- if (input$land_valg_2 == "Holland") dfskud_holland else dfskud_polen
+    
+    kamp_data %>%
+      filter(label == input$Kamp_filter) %>%
+      group_by(team.name) %>%
+      summarise(
+        `Skud total` = n(),
+        `Skud på mål` = sum(shot.onTarget, na.rm = TRUE),
+        Mål = sum(shot.isGoal, na.rm = TRUE),
+        `Mål i procent af skud` = Mål / `Skud total` * 100
+      )
+  })
+  
+  output$skud_table <- renderTable({
+    filtered_data_4_skud()
+  })
+  
+}
+
+# Kør appen
+shinyApp(ui, server)
